0ee4e566d3c42db1229c15fe9f82d8055bbefd2a
Garrett Kembel <garrett_kembel1@baylor.edu>
2024-02-08 13:19:57 -0600

    employee report

--- employee_report.py
+++ employee_report.py
@@ -29,16 +29,17 @@ for rec in csv_file:
         low_efficient_individuals.append(name)
 
 
-    # Count employees by age group and store names
-    if 30 <= age <= 39:
+    
+    if age >= 40:
+        count_40 += 1
+        employee_40.append(name)
+    elif 30 <= age <= 39:
         count_30 += 1
         employee_30.append(name)
     elif 20 <= age <= 29:
         count_20 += 1
         employee_20.append(name)
-    elif age >= 40:
-        count_40 += 1
-        employee_40.append(name)
+    
 
 
 print('Highly Efficient Individuals:')
@@ -51,21 +52,21 @@ for name in low_efficient_individuals:
     print(name)
 print('\n')
 
-# Print employees in their 40s and count
+
 print('Employees in their 40s:')
 for name in employee_40:
     print(name)
 print('\n')
 print(f"Total Number of Employees in their 40's: {count_40}",'\n')
 
-# Print employees in their 30s and count
+
 print('Employees in their 30s:')
 for name in employee_30:
     print(name)
 print('\n')
 print(f"Total Number of Employees in their 30's: {count_30}",'\n')
 
-# Print employees in their 20s and count
+
 print('Employees in their 20s:')
 for name in employee_20:
     print(name)
