d6d7ef7bbc481fc944cda6225b80e8614a7e36ef
Garrett Kembel <garrett_kembel1@baylor.edu>
2024-02-08 13:19:35 -0600

    Files HW 2

--- employee_report.py
+++ employee_report.py
@@ -7,22 +7,69 @@ csv_file = csv.reader(employee_data)
 #this will skip the first record which is the header
 next(csv_file)
 
+highly_efficient_individuals = []
+low_efficient_individuals = []
+
+employee_40 = []
+employee_30 = []
+employee_20 = []
+
+count_20 = 0
+count_30 = 0
+count_40 = 0
 
 for rec in csv_file:
     efficiency = float(rec[5])/float(rec[4])
-    age = float(rec[2])
-    #bonus = float(rec[7]) * salary
-    #pay = salary + bonus
-    #print(rec)
-    if efficiency >2:
-        print('Highly Efficent Individuals','\n')
-        print(rec[1])
-        #print(f'Salary: $ {salary:.2f}')
-        #print(f'Bonus: $ {bonus:.2f}')
-        #print(f'Pay: $ {pay:.2f}')
-    elif efficiency <1:
-        print('Low Efficency Individuals','\n')
-        print(rec[1])
-
+    age = int(rec[2])
+    name = rec[1]
     
-    if age
+    if efficiency >= 2:
+        highly_efficient_individuals.append(name)
+    elif efficiency <= 1:
+        low_efficient_individuals.append(name)
+
+
+    # Count employees by age group and store names
+    if 30 <= age <= 39:
+        count_30 += 1
+        employee_30.append(name)
+    elif 20 <= age <= 29:
+        count_20 += 1
+        employee_20.append(name)
+    elif age >= 40:
+        count_40 += 1
+        employee_40.append(name)
+
+
+print('Highly Efficient Individuals:')
+for name in highly_efficient_individuals:
+    print(name)
+print('\n')
+
+print('Low Efficiency Individuals:')
+for name in low_efficient_individuals:
+    print(name)
+print('\n')
+
+# Print employees in their 40s and count
+print('Employees in their 40s:')
+for name in employee_40:
+    print(name)
+print('\n')
+print(f"Total Number of Employees in their 40's: {count_40}",'\n')
+
+# Print employees in their 30s and count
+print('Employees in their 30s:')
+for name in employee_30:
+    print(name)
+print('\n')
+print(f"Total Number of Employees in their 30's: {count_30}",'\n')
+
+# Print employees in their 20s and count
+print('Employees in their 20s:')
+for name in employee_20:
+    print(name)
+print('\n')
+print(f"Total Number of Employees in their 20's: {count_20}")
+
+   
